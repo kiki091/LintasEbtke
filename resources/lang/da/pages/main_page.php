5ffe119b5a9667c57d39e4edeeacbf6a59afab6b
kiki091 <kikikurniawan091@gmail.com>
2017-06-09 01:02:56 +0700

    update lang static denmark

--- resources/lang/da/pages/main_page.php
+++ resources/lang/da/pages/main_page.php
@@ -3,21 +3,21 @@
 	return [
 		'title_news' => 'Seneste Nyt',
 		'title_we_do' => 'HVAD VI GØR ?',
-		'sub_desc_we_do' => 'To full fill our vision and mission we provide the following services',
-		'title'  	=> 'Whats is lintas ?',
-		'introduction' 	=> 'LINTAS EBTKE is an extension of the New and Renewable Energy Information and Investment Service and Energy Conservation.',
+		'sub_desc_we_do' => 'For at opfylde vores vision og mission leverer vi følgende tjenester',
+		'title'  	=> 'Hvad er lintas ?',
+		'introduction' 	=> 'LINTAS EBTKE er en udvidelse af den nye og vedvarende energi informations- og investeringstjeneste og energibesparelse.',
 		'introduction_left' => '<p>
-			<span class="first-letter">I</span>
-			n general, LINTAS EBTKE will become the axis of EBTKE information where the information technology system applied will help the visitor find the information they need quickly. In addition, the EBTKE LINTAS office enables people to visit and conduct face-to-face consultations with competent staff and assigned to EBTKE Traffic.
+			<span class="first-letter">G</span>
+			enerelt vil LINTAS EBTKE blive EBTKE-aksen, hvor det anvendte informationsteknologisystem hjælper den besøgende med at finde de oplysninger, de har brug for hurtigt. Desuden giver EBTKE LINTAS-kontoret folk mulighed for at besøge og gennemføre ansigt-til-ansigt-konsultationer med kompetent personale og tildelt EBTKE Traffic.
 			</p>
 			<p>
-			Only a small part of Indonesia renewable energy potential and energy conservation have been utilized. To optimize the utilization of such potentials, communication and close cooperation between stakeholders is essential so that EBTKE activities / projects can be implemented immediately.. 
+			Kun en lille del af Indonesiens vedvarende energipotentiale og energibesparelser er blevet udnyttet. For at optimere udnyttelsen af sådanne potentialer er kommunikation og tæt samarbejde mellem interessenter af afgørende betydning, så EBTKE-aktiviteter / projekter kan implementeres straks.. 
 			</p>',
 		'introduction_right' 	=> '<p>
-			It is suspected that there are still unmet needs of local government, industry, project development, univeersitas and research institutes, investors, financial institutions and the general public for information policy and EBTKE implementation. These stakeholders need the best and most current information on the applicable regulations, technology and implementation, funding and investment, cooperation opportunities, and even technical assistance for planning the implementation of EBTKE.
+			Det er mistanke om, at der stadig mangler behov for kommuner, industri, projektudvikling, univeersitas og forskningsinstitutter, investorer, finansielle institutioner og offentligheden for informationspolitik og EBTKE-implementering. Disse interessenter har brug for den bedste og nyeste information om gældende regler, teknologi og implementering, finansiering og investering, samarbejdsmuligheder og endda teknisk bistand til planlægning af implementeringen af EBTKE.
 			</p>
 			<p>
-				Information from LINTAS EBTKE is expected to facilitate cooperation among these stakeholders so that the utilization and application of EBTKE can increase.
+				Oplysninger fra LINTAS EBTKE forventes at lette samarbejdet mellem disse interessenter, således at brugen og anvendelsen af EBTKE kan øge.
 			</p>',
 		'title_consulting_services' 	=> 'Konsulenttjenester',
 		'title_news_&_events' 			=> 'NYHEDER OG ARRANGEMENTER',
