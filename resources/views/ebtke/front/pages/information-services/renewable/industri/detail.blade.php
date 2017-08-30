9c7b909b409581972aeb65e1528f6f0e474692e5
kiki <kikikurniawan091@gmail.com>
2017-08-30 19:51:34 +0700

    fix bugs mobile version tabel data industri

--- resources/views/ebtke/front/pages/information-services/renewable/industri/detail.blade.php
+++ resources/views/ebtke/front/pages/information-services/renewable/industri/detail.blade.php
@@ -65,30 +65,114 @@
     </div>
     <div class="container">
         <div class="row">
-            <div class="col-md-12">
+            <div class="cd-tabs col-md-12">
                 <nav>
                     <ul class="cd-tabs-navigation">
                         <li>
-                            <a target="_blank" href="{{ route('CertifiedEnergy') }}">
+                            <a data-content="Industri" class="selected" target="_blank" href="javascript:void(0);">
+                                Industry
+                            </a>
+                        </li>
+                        <li>
+                            <a data-content="CertifiedEnergy" target="_blank" href="javascript:void(0);">
                                 Certified Energy
                             </a>
                         </li>
                         <li>
-                            <a target="_blank" href="{{ route('EnergyAuditor') }}">
+                            <a data-content="EnergyAuditor" target="_blank" href="javascript:void(0);">
                                 Energy Auditor
                             </a>
                         </li>
                     </ul> <!-- cd-tabs-navigation -->
                 </nav>
+                <ul class="cd-tabs-content">
+                    <li data-content="Industri" class="selected">
+                        <div class="default-content">
+                            {!! $detail_industri['introduction'] or '' !!}
+                            {!! $detail_industri['description'] or '' !!}
+                        
+                        </div>
+                    </li>
+                    <li data-content="CertifiedEnergy">
+                        <div class="default-content">
+                          <h3>List data Certified Energy</h3>
+                        </div>
+                        <table cellpadding="0" cellspacing="0" class="table--data-style">
+                            <tbody>
+                                <tr id="desktop__tabel">
+                                    <td class="header">Fullname</td>
+                                    <td class="header">Company Name</td>
+                                    <td class="header">Province Name</td>
+                                    <td class="header">Sector</td>
+                                    <td class="header">Sub Sector</td>
+                                </tr>
+                                <tr id="mobile__tabel">
+                                    <td class="header">Company Name</td>
+                                    <td class="header">Sector</td>
+                                    <td class="header">Sub Sector</td>
+                                </tr>
+                                @if(isset($certified_energy) && !empty($certified_energy))
+                                @foreach($certified_energy as $key => $certified_energy)
+                                <tr id="desktop__tabel">
+                                    <td>{{ $certified_energy['fullname'] }}</td>
+                                    <td>{{ $certified_energy['company_name'] }}</td>
+                                    <td>{{ $certified_energy['province_name'] }}</td>
+                                    <td>{{ $certified_energy['sector'] }}</td>
+                                    <td>{{ $certified_energy['sub_sector'] }}</td>
+                                </tr>
+                                <tr id="mobile__tabel">
+                                    <td>{{ $certified_energy['company_name'] }}</td>
+                                    <td>{{ $certified_energy['sector'] }}</td>
+                                    <td>{{ $certified_energy['sub_sector'] }}</td>
+                                </tr>
+                                @endforeach
+                                @endif
+                            </tbody>
+                        </table>
+                    </li>
+                    <li data-content="EnergyAuditor">
+                        <div class="default-content">
+                          <h3>List data Energy Auditor</h3>
+                        </div>
+                        <table cellpadding="0" cellspacing="0" class="table--data-style">
+                            <tbody>
+                                <tr id="desktop__tabel">
+                                    <td class="header">Fullname</td>
+                                    <td class="header">Company Name</td>
+                                    <td class="header">Province Name</td>
+                                    <td class="header">Type Auditor</td>
+                                    <td class="header">Years</td>
+                                    <td class="header">Sector</td>
+                                    <td class="header">Sub Sector</td>
+                                </tr>
+                                <tr id="mobile__tabel">
+                                    <td class="header">Company Name</td>
+                                    <td class="header">Type Auditor</td>
+                                    <td class="header">Years</td>
+                                </tr>
+                                @if(isset($energy_auditor) && !empty($energy_auditor))
+                                @foreach($energy_auditor as $key => $energy_auditor)
+                                <tr id="desktop__tabel">
+                                    <td>{{ $energy_auditor['fullname'] }}</td>
+                                    <td>{{ $energy_auditor['company_name'] }}</td>
+                                    <td>{{ $energy_auditor['province_name'] }}</td>
+                                    <td>{{ $energy_auditor['type_auditor'] }}</td>
+                                    <td>{{ $energy_auditor['years'] }}</td>
+                                    <td>{{ $energy_auditor['sector'] }}</td>
+                                    <td>{{ $energy_auditor['sub_sector'] }}</td>
+                                </tr>
+                                <tr id="mobile__tabel">
+                                    <td>{{ $energy_auditor['company_name'] }}</td>
+                                    <td>{{ $energy_auditor['type_auditor'] }}</td>
+                                    <td>{{ $energy_auditor['years'] }}</td>
+                                </tr>
+                                @endforeach
+                                @endif
+                            </tbody>
+                        </table>
+                    </li>
+                </ul>
             </div>
-        </div>
-    </div>
-    <div class="container style--texteditor">
-
-        <div class="default-content">
-            {!! $detail_industri['introduction'] or '' !!}
-            {!! $detail_industri['description'] or '' !!}
-        
         </div>
     </div>
 </section>
